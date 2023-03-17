f15c47b02809eb56b8bf0c4e80529a0128ec6629
JiinArcade <jn_ll@naver.com>
2023-03-17 14:59:08 +0900

    💄

--- 230317/src/App.js
+++ 230317/src/App.js
@@ -8,30 +8,29 @@ import ProductDetail from './Router/ProductDetail'
 
 function App() {
   return (
-    <div className="App">
-      <Routes>
-        {/* Route 안에 속성값이 두 개가 들어간다 path / element 
+    <Routes>
+      {/* Route 안에 속성값이 두 개가 들어간다 path / element 
             1. path : 페이지의 주소 URL
             2. element : path 주소로 이동 했을 때 보여줄 페이지
             / : 기본 경로
         */}
-        <Route path='/' element={<Home />} />
+      <Route path='/' element={<Home />} />
 
-        {/* nested Routes */}
-        {/* Outlet */}
-        <Route path='/about' element={<About />}>
-          <Route path='member' element={<h1>멤버~~</h1>} />
-          <Route path='location' element={<h1>로케이션~~!</h1>} />
-        </Route>
+      {/* nested Routes */}
+      {/* Outlet */}
+      <Route path='/about' element={<About />}>
+        <Route path='member' element={<h1>이거뭐지</h1>} />
+        <Route path='location' element={<h1>ss</h1>} />
+      </Route>
 
-        <Route path='/about' element={<About />} />
-        <Route path='/about/member' element={<h1>멤버~~</h1>} />
-        <Route path='/about/location' element={<h1>로케이션~~!</h1>} />
+      <Route path='/about' element={<About />} />
+      <Route path='/about/member' element={<h1>이거뭐지</h1>} />
+      <Route path='/about/location' element={<h1>ss</h1>} />
 
-        <Route path='/products' element={<Products />} />
-        <Route path='/products/:id' element={<ProductDetail />} />
-      </Routes>
-    </div>
+      <Route path='/products' element={<Products />} />
+      <Route path='/products/:id' element={<ProductDetail />} />
+    </Routes>
+
   );
 }
 
